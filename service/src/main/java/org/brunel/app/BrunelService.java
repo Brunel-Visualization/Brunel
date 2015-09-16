e981f72d0810424da15846d77631b467fec42bb3
DCO 1.1 Daniel Rope <drope@us.ibm.com>
2015-09-16 10:10:33 -0400

    Clean up.  Removing demo code no longer used, better docs for BrunelService.

--- service/src/main/java/org/brunel/app/BrunelService.java
+++ service/src/main/java/org/brunel/app/BrunelService.java
@@ -17,20 +17,7 @@
 
 package org.brunel.app;
 
-import org.brunel.action.Action;
-import org.brunel.build.Builder;
-import org.brunel.build.controls.ControlWriter;
-import org.brunel.build.controls.Controls;
-import org.brunel.build.d3.D3Builder;
-import org.brunel.build.util.ContentReader;
-import org.brunel.build.util.DataCache;
-import org.brunel.data.Dataset;
-import org.brunel.data.io.CSV;
-import org.brunel.match.BestMatch;
-import org.brunel.model.VisItem;
-import org.brunel.util.WebDisplay;
-
-
+import java.io.IOException;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
@@ -45,13 +32,25 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import java.io.IOException;
-import java.io.InputStream;
+import org.brunel.action.Action;
+import org.brunel.build.Builder;
+import org.brunel.build.controls.ControlWriter;
+import org.brunel.build.controls.Controls;
+import org.brunel.build.d3.D3Builder;
+import org.brunel.build.util.ContentReader;
+import org.brunel.build.util.DataCache;
+import org.brunel.data.Dataset;
+import org.brunel.data.io.CSV;
+import org.brunel.match.BestMatch;
+import org.brunel.model.VisItem;
+import org.brunel.util.WebDisplay;
 
 /**
- * Sample JAX-RS web application that produces d3 JS/CSS.
+ * Sample JAX-RS web application that produces Brunel visualizations.  Currently only d3 output is supported.
+ * Service methods are provided to create the raw Brunel content (JS/CSS) or a full HTML page.  An additional service
+ * method can generate Brunel that shows a given visualization on new data.
  * <p/>
- * REST pattern is:
+ * REST pattern for raw content is:
  * <p/>
  * POST /brunel/interpret/d3?src={brunel}&width=..&height=..
  * <p/>
@@ -62,52 +61,20 @@ import java.io.InputStream;
 @Path("interpret")
 public class BrunelService extends Application {
 
-	//TODO:  Will be removed
-//    @POST
-//    @Path("data")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    //For data uploading.  Note, data stored in memory for the moment.
-
-//    public void addSource(@Multipart("file") Attachment attachment, @Multipart("file_name") String fileName) {
-//        try {
-//            // The getting part caches it
-//            InputStream is = attachment.getObject(InputStream.class);
-//            DataCache.get(fileName, is);
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//    }
-
-    @GET
-    @Path("match")
-    @Produces(MediaType.TEXT_PLAIN)
-
-    //Get action on new data using previous data and supplied action
-    public String actionFromExisting(@QueryParam("original_data") String originalData,
-                                     @QueryParam("new_data") String newData,
-                                     @QueryParam("src") String brunelSrc) {
-        try {
-        	if (originalData != null) {
-	            Dataset origDS = DataCache.get(originalData);
-	            Dataset newDS = DataCache.get(newData);
-	            return BestMatch.match(origDS, newDS, Action.parse(brunelSrc)).toString();
-        	}
-        	else {
-        		return BestMatch.match(brunelSrc, newData).toString();
-        	}
-        } catch (IOException e) {
-            // You would have to be really unlucky ot get this -- the cache would have to be flushed and then the
-            // the remote fle fail to be read.
-            throw makeException("Could not read data for match: " + e.getMessage(), Status.BAD_REQUEST.getStatusCode());
-        }
-    }
-
+    /**
+     * Generates all JS/CSS using D3 to produce a visualization.  The data can be on the payload or it can be specified using
+     * the Brunel data() function.
+     * @param data   the data to use for the visualization (as URL or cache identifier)
+     * @param brunelSrc the Brunel syntax defining the visualization
+     * @param width the desired width of the resulting visualization
+     * @param height the desired height of the resulting visualization
+     * @param visId an identifier to use for the d3 JS to reference the HTML tag containing the visualization on the web page (usually an SVG tag).  
+     * @return a JSON object containing the css, js, and an object describing interactive controls that require a separate UI
+     */
     @POST
-    @Path("d3")
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("d3")							   
+    @Consumes(MediaType.TEXT_PLAIN)        //A CSV file is the payload
     @Produces(MediaType.APPLICATION_JSON)  //JSON object with "js" and "css" entries
-    //Note, this call requires an identifier to use for the d3 JS to reference the HTML tag (usually an SVG tag).
     public D3Result createAsD3(String data, @QueryParam("src") String brunelSrc,
                                @QueryParam("width") int width,
                                @QueryParam("height") int height,
@@ -120,53 +87,20 @@ public class BrunelService extends Application {
         return result;
     }
 
-    private Builder makeD3(Dataset data, String actionText, int width, int height, String visId) {
-
-        try {
-            D3Builder builder = new D3Builder(visId);
-            VisItem item = makeVisItem(data, actionText);
-            builder.build(item, width, height);
-            return builder;
-        } catch (Exception ex) {
-        	ex.printStackTrace();
-            throw makeException("Could not execute Brunel: " + actionText + ": " + ex.getMessage(), Status.BAD_REQUEST.getStatusCode());
-        }
-    }
-
-    private Dataset makeBrunelData(String data) {
-        try {
-            return  Dataset.make(CSV.read(data));
-        } catch (Exception e) {
-            throw makeException("Could not create data as CSV from content", Status.BAD_REQUEST.getStatusCode());
-        }
-    }
-
-
-    private Dataset readBrunelData(String url) {
-        try {
-            return DataCache.get(url);
-        } catch (Exception e) {
-            throw makeException("Could not read data as CSV from: " + url, Status.BAD_REQUEST.getStatusCode());
-        }
-    }
-
-    private VisItem makeVisItem(Dataset brunel, String actionText) {
-        Action action = Action.parse(actionText);
-        return action.apply(brunel);
-    }
-
-    private WebApplicationException makeException(String message, int code) {
-
-        return new WebApplicationException(
-                Response.status(Status.fromStatusCode(code))
-                        .entity(message).type(MediaType.TEXT_PLAIN).build());
-    }
-
+    /**
+     * Creates a full HTML page suitable for use within an HTML IFrame.
+     * @param brunelSrc the Brunel syntax defining the visualization
+     * @param brunelUrl (optional) a URL to a file containing the Brunel syntax
+     * @param width the desired width of the resulting visualization
+     * @param height the desired height of the resulting visualization
+     * @param dataUrl a URL pointing to the CSV to use for the visualization's data.  Note if the Brunel contains a data()
+     *  function, then this will be used instead
+     * @param filesLoc (optional) an alternate location for the main Brunel javascript
+     * @return a full HTML page with all JS/CSS and interactive controls for a given visualization.
+     */
     @GET
     @Path("d3")
     @Produces(MediaType.TEXT_HTML)
-
-
     public String createAsD3Html(@QueryParam("brunel_src") String brunelSrc,
     							 @QueryParam("brunel_url") String brunelUrl,
                                  @QueryParam("width") int width,
@@ -193,5 +127,85 @@ public class BrunelService extends Application {
     	}
 
     }
+
+    
+	/**
+	 * Service that creates new Brunel syntax to use a given visualization with new data.
+	 * @param originalData the original data (as URL or cache identifier)
+	 * @param newData the new data (as URL or cache identifier)
+	 * @param brunelSrc the Brunel syntax that produced the original visualization
+	 * @return Brunel syntax using the new data 
+	 */
+    @GET
+    @Path("match")
+    @Produces(MediaType.TEXT_PLAIN)
+
+    public String actionFromExisting(@QueryParam("original_data") String originalData,
+                                     @QueryParam("new_data") String newData,
+                                     @QueryParam("src") String brunelSrc) {
+        try {
+        	if (originalData != null) {
+	            Dataset origDS = DataCache.get(originalData);
+	            Dataset newDS = DataCache.get(newData);
+	            return BestMatch.match(origDS, newDS, Action.parse(brunelSrc)).toString();
+        	}
+        	else {
+        		return BestMatch.match(brunelSrc, newData).toString();
+        	}
+        } catch (IOException e) {
+            // You would have to be really unlucky to get this -- the cache would have to be flushed and then the
+            // the remote fle fail to be read.
+            throw makeException("Could not read data for match: " + e.getMessage(), Status.BAD_REQUEST.getStatusCode());
+        }
+    }
+
+
+    //Creates a D3Builder to produce the d3 output
+    private Builder makeD3(Dataset data, String actionText, int width, int height, String visId) {
+
+        try {
+            D3Builder builder = new D3Builder(visId);
+            VisItem item = makeVisItem(data, actionText);
+            builder.build(item, width, height);
+            return builder;
+        } catch (Exception ex) {
+        	ex.printStackTrace();
+            throw makeException("Could not execute Brunel: " + actionText + ": " + ex.getMessage(), Status.BAD_REQUEST.getStatusCode());
+        }
+    }
+
+    //Create a Dataset instance given CSV
+    private Dataset makeBrunelData(String data) {
+        try {
+            return  Dataset.make(CSV.read(data));
+        } catch (Exception e) {
+            throw makeException("Could not create data as CSV from content", Status.BAD_REQUEST.getStatusCode());
+        }
+    }
+
+
+    //Get a Dataset instance given a URL.  The content will be loaded if not present in the cache.
+    private Dataset readBrunelData(String url) {
+        try {
+            return DataCache.get(url);
+        } catch (Exception e) {
+            throw makeException("Could not read data as CSV from: " + url, Status.BAD_REQUEST.getStatusCode());
+        }
+    }
+
+    //Create the VisItem instance for the given Brunel
+    private VisItem makeVisItem(Dataset brunel, String actionText) {
+        Action action = Action.parse(actionText);
+        return action.apply(brunel);
+    }
+
+    //Simple web exception handling
+    private WebApplicationException makeException(String message, int code) {
+
+        return new WebApplicationException(
+                Response.status(Status.fromStatusCode(code))
+                        .entity(message).type(MediaType.TEXT_PLAIN).build());
+    }
+
 
 }
