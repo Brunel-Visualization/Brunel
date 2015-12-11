3c57d8f67bd84c1aeea874fd22dfe306ebf14fa9
Graham J. Wills <gwills@us.ibm.com>
2015-12-11 15:01:13 -0600

    Initial Version
    DCO 1.1 Signed-off-by: Graham Wills <gwills@us.ibm.com>

--- core/src/main/java/org/brunel/build/d3/diagrams/Network.java
+++ core/src/main/java/org/brunel/build/d3/diagrams/Network.java
@@ -32,7 +32,6 @@ class Network extends D3Diagram {
         this.dependency = dependency;
     }
 
-
     public ElementDetails writeDataConstruction() {
         String nodeField = quoted(vis.fKeys.get(0).asField());
 
@@ -43,7 +42,16 @@ class Network extends D3Diagram {
 
         out.add("chart.graph = BrunelData.diagram_Graph.make(processed,", nodeField, ",",
                 edgeDataset, ",", a, ",", b, ")").endStatement();
+        makeLayout();
         return ElementDetails.makeForDiagram("chart.graph.nodes", "circle", "point", "box", false);
+    }
+
+    private void makeLayout() {
+        out.comment("Initial Circle Layout")
+                .add("var g = chart.graph, r1 = geom.inner_width/2, r2 = geom.inner_height/2,").onNewLine()
+                .add("a, i, N = g.nodes.length").endStatement()
+                .add("for(i=0; i<N; i++) { a = Math.PI*2*i/N; g.nodes[i].x = r1 + 0.75*r1*Math.cos(a); g.nodes[i].y = r2 + 0.75*r2*Math.sin(a) }")
+                .endStatement();
     }
 
     public void writeDefinition(ElementDetails details, ElementDefinition elementDef) {
