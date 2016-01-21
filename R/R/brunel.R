b34040367327769c335b3915ff578e5c68834b1c
DCO 1.1 Daniel Rope <drope@us.ibm.com>
2016-01-21 09:12:45 -0500

    R notebook updates.  Widget code no longer needed, support for multiple data() statements.
    Signed-off-by: DCO 1.1 Daniel Rope <drope@us.ibm.com>

--- R/R/brunel.R
+++ R/R/brunel.R
@@ -15,6 +15,10 @@
 d3_template_html <-  readLines(system.file("templates", "D3_Template.html", package = "brunel"))
 brunel_server <- Sys.getenv("BRUNEL_SERVER")
 
+#sessionid is needed since the R implementation currently uses Brunel web service which caches the data.
+#This should avoid problems with same named dataframes in different notebooks.
+sessionid <- paste("",uuid::UUIDgenerate(),sep='')
+
 #' Produce a Brunel visualization
 #'
 #' Given a brunel script and a supplied data frame, renders the visualization.
@@ -26,16 +30,18 @@ brunel_server <- Sys.getenv("BRUNEL_SERVER")
 #' width the width in pixels for the resulting visualization
 #' height the heigh in pixels for the resulting visualization
 
-brunel <- function(brunel, data, width=800, height=600) {
+brunel <- function(brunel, data=NULL, width=800, height=600) {
+    cache_data(brunel)
 	visid <-  paste("vis", uuid::UUIDgenerate(), sep='')
 	controlsid <- paste("controls", uuid::UUIDgenerate(), sep='')
-	query_params = list(src=brunel, width=width, height=height, visid=visid)
+	query_params = list(src=brunel, width=width, height=height, visid=visid, controlsid=controlsid, data_prefix=sessionid)
 	csv <- to_csv(data)
 	response <- brunel_service_call(query_params, csv)
 	display_d3_output(response, visid, controlsid, width, height)	
 }
 
 to_csv <- function(data) {
+    if (is.null(data)) return(NULL)
 	tc <- textConnection("out", "w") 
 	write.csv(data, tc, row.names =FALSE)	
 	close(tc)
@@ -45,6 +51,27 @@ to_csv <- function(data) {
 brunel_service_call <- function (query_params, csv) {
 	url = paste(brunel_server, "/brunel/interpret/d3", sep='')	
 	response <- httr::POST(url, query = query_params, httr::content_type("text/plain"), httr::accept_json(), body=csv) 
+}
+
+get_dataset_names <- function (brunel) {
+    query_params <- list(brunel_src=brunel)
+    url = paste(brunel_server, "/brunel/interpret/data_names", sep='')
+    response <- httr::GET(url, query = query_params,  httr::accept_json()) 
+    con <- httr::content(response)
+}
+
+cache_data <- function (brunel) {
+    data_set_names <- get_dataset_names(brunel)
+    for (name in data_set_names) {
+        obj <- get(name)
+        if (class(obj) == "data.frame") {
+            query_params <- list(data_key=name, prefix=sessionid)
+            csv <- to_csv(obj)
+            url <- paste(brunel_server, "/brunel/interpret/cache", sep='')
+            response <- httr::POST(url, query = query_params, httr::content_type("text/plain"), body=csv) 
+        }
+    }
+    
 }
 
 display_d3_output <- function(response, visid, controlsid, width, height) {
@@ -57,9 +84,8 @@ display_d3_output <- function(response, visid, controlsid, width, height) {
 	else {
 		d3js <- con$js
       	d3css <- con$css
-      	controls <- con$controls
 
-		html_values <- list(d3css=d3css, visId=visid, width=width, height=height, d3js=d3js, controlsid=controlsid, controls=build_brunel_widget_js(controls, controlsid))
+		html_values <- list(d3css=d3css, visId=visid, width=width, height=height, d3js=d3js, controlsid=controlsid)
 		html <- template_render(d3_template_html, html_values)
 		IRdisplay::display_html(html)
   	}
