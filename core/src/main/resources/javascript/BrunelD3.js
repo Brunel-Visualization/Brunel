e12a5c538f6989f27476e20eaf0656065d469673
Graham J. Wills <gwills@us.ibm.com>
2016-05-10 14:18:55 -0500

    Label speed improvement

--- core/src/main/resources/javascript/BrunelD3.js
+++ core/src/main/resources/javascript/BrunelD3.js
@@ -17,8 +17,8 @@
 // A closure for the utilities needed to build Brunel items for D3
 var BrunelD3 = (function () {
 
-	//Ensure topojson is loaded under AMD.  Unclear why this is needed for topojson, but not d3.
-	if (typeof topojson === 'undefined' && typeof require === 'function' ) topojson = require('topojson');
+    //Ensure topojson is loaded under AMD.  Unclear why this is needed for topojson, but not d3.
+    if (typeof topojson === 'undefined' && typeof require === 'function') topojson = require('topojson');
 
     var tooltip, lastTime, lastTimeDescr;
     // Return geometries for the given target given the desired margins
@@ -628,20 +628,24 @@ var BrunelD3 = (function () {
             // Otherwise we get flickering due to different rounding of the panned coordinates
             hits.x = box.x;
             hits.y = box.y;
+            hits.n = 0;
         }
 
-        var D = 1;
+
+        // Set the grid size as a power of 2 depending how mch data we have seen
+        var n = Math.log(((++hits.n) / 10)) / Math.log(4), D = Math.pow(2, Math.floor(n));
+        D = Math.min(16, Math.max(1, D));
 
         var i, j, x = box.x - hits.x, y = box.y - hits.y,
-            xmin = Math.round(x / D), xmax = Math.round((x + box.width) / D),
-            ymin = Math.round(y / D), ymax = Math.round((y + box.height) / D);
+            xmin = D * Math.ceil(x / D), xmax = x + box.width,
+            ymin = D * Math.ceil(y / D), ymax = y + box.height;
 
         // Does it hit an existing location
-        for (i = xmin; i <= xmax; i++) for (j = ymin; j <= ymax; j++)
+        for (i = xmin; i <= xmax; i += D) for (j = ymin; j <= ymax; j += D)
             if (hits[i * 10000 + j]) return true;
 
         // No! so we must update those locations before returning the fact it misses
-        if (update) for (i = xmin; i <= xmax; i++) for (j = ymin; j <= ymax; j++)
+        if (update) for (i = xmin; i <= xmax; i += D) for (j = ymin; j <= ymax; j += D)
             hits[i * 10000 + j] = true;
 
         return false;
